67c97903fbc8c458f290f36368db1dafcd33bf00
Simon Rosén <simon.rosen@elev.ga.ntig.se>
2024-09-10 10:58:50 +0200

    klassen

---
+++ TamagotchiClass.cs
@@ -0,0 +1,36 @@
+public class Tamagotchi{
+    private int hunger;
+    private int boredom;
+    private list<string> words = new;
+    private bool isAlive;
+    private Random generator = new;
+    public string name;
+
+    public void Feed(){
+
+    }
+
+    public void Hi(){
+
+    }
+
+    public void Teach(string word){
+
+    }
+
+    public void Tick(){
+
+    }
+
+    public void PrintStats(){
+
+    }
+
+    public bool GetAlive(){
+
+    }
+
+    private void ReduceBoredom(){
+        
+    }
+}
